087ae55e2dfbcc569694fa87329438eae66889f9
Yoshinori Yasuda <yasuda.yoshinori@gmail.com>
2020-09-29 14:48:01 +0900

    a^n modとa^(-1) modを計算

---
+++ modinv.rb
@@ -0,0 +1,15 @@
+# a^n mod を計算
+def modpow(a, n, mod)
+  res = 1
+  while n > 0
+    res = res * a % mod if n & 1 != 0
+    a = a * a % mod
+    n >>= 1
+  end
+  res
+end
+
+# a^{-1} mod を計算
+def modinv(a, mod)
+  modpow(a, mod - 2, mod)
+end
