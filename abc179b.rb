a78dad56a6d958842dcb86f6c681db2780757138
Yoshinori Yasuda <yasuda.yoshinori@gmail.com>
2020-10-07 10:40:01 +0900

    abc179b.rb: ブロック付きメソッド呼び出しに変更

--- abc179b.rb
+++ abc179b.rb
@@ -2,7 +2,7 @@ n = gets.chomp.to_i
 count = 0
 result = 'No'
 n.times do
-  d1, d2 = gets.chomp.split(' ').map {|d| d.to_i}
+  d1, d2 = gets.chomp.split(' ').map(&:to_i)
   count = d1 == d2 ? count + 1 : 0
   if count >= 3
     result = 'Yes'
